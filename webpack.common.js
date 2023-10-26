3ad0f7b28b460a84c19cfc57b534cb8fbff666de
RaulPabianCruz <ito.pabian@gmail.com>
2023-10-25 19:03:58 -0700

    Add css loader to webpack configuration

--- webpack.common.js
+++ webpack.common.js
@@ -11,6 +11,22 @@ module.exports = {
             scriptLoading: 'defer',
         })
     ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },
+        ],
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
