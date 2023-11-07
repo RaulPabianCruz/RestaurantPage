4214665c887fac6d428ba0b27e75736700299349
RaulPabianCruz <ito.pabian@gmail.com>
2023-11-07 13:01:19 -0800

    Fix issue with styling being absent from index.html
    Fixes issue where the styling of the restaurant page was absent in the
    production/distribution build of the project.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { makeHeader } from './header.js';
 import { makeHomePage } from './home.js';
 import { makeContactPage } from './contact.js';
 import { makeMenuPage } from './menu.js';
+import './style.css';
 
 const container = document.getElementById('content');
 container.appendChild(makeHeader());
